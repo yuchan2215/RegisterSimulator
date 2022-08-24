63c636ae665cf28e09ae91d48d6670f6e39d7903
Miyayu <yuchan2215@gmail.com>
2022-08-24 15:15:33 +0900

    add: ボタンを押すとレジ画面へ移動できるように

---
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/RegisterTopFragment.kt
@@ -0,0 +1,7 @@
+package xyz.miyayu.android.registersimulator.views.fragments
+
+import androidx.fragment.app.Fragment
+import xyz.miyayu.android.registersimulator.R
+
+class RegisterTopFragment : Fragment(R.layout.register_top_fragment) {
+}
--- app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/TopFragment.kt
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/TopFragment.kt
@@ -1,7 +1,17 @@
 package xyz.miyayu.android.registersimulator.views.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import xyz.miyayu.android.registersimulator.R
+import xyz.miyayu.android.registersimulator.databinding.TopFragmentBinding
 
 class TopFragment : Fragment(R.layout.top_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = TopFragmentBinding.bind(view)
+        binding.openRegisterButton.setOnClickListener {
+            view.findNavController().navigate(TopFragmentDirections.openRegister())
+        }
+    }
 }
