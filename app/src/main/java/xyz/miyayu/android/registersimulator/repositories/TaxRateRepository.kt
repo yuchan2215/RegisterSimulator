5d8fc6cbb9d0debf580bc39c119deb0b2c3b56de
Miyayu <yuchan2215@gmail.com>
2022-08-24 23:47:23 +0900

    add: 税率のリポジトリを作成

---
+++ app/src/main/java/xyz/miyayu/android/registersimulator/repositories/TaxRateRepository.kt
@@ -0,0 +1,25 @@
+package xyz.miyayu.android.registersimulator.repositories
+
+import xyz.miyayu.android.registersimulator.RegisterApplication
+import xyz.miyayu.android.registersimulator.model.entity.TaxRate
+
+object TaxRateRepository {
+    private val default_tax_rates = listOf(
+        TaxRate(id = 0, title = "標準税率", rate = "10"),
+        TaxRate(id = 1, title = "軽減税率", rate = "8"),
+        TaxRate(id = 2, title = "予備税率", rate = "20")
+    )
+
+    private fun getTaxRateDao() = RegisterApplication.instance.database.taxRateDao()
+
+    /**
+     * 税率一覧を取得します。
+     * もしデータベース上に存在しない場合は、デフォルトの設定を利用します。
+     */
+    suspend fun getTaxRates(): List<TaxRate> {
+        val dao = getTaxRateDao()
+        return default_tax_rates.map {
+            return@map dao.getTaxRate(it.id) ?: it
+        }
+    }
+}
