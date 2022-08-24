e7b03b52d93b9c214469a006c9849a55e13aeb9e
Miyayu <yuchan2215@gmail.com>
2022-08-24 14:02:45 +0900

    add: トップ画面のクラスを作成

---
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/TopFragment.kt
@@ -0,0 +1,7 @@
+package xyz.miyayu.android.registersimulator.views.fragments
+
+import androidx.fragment.app.Fragment
+import xyz.miyayu.android.registersimulator.R
+
+class TopFragment : Fragment(R.layout.top_fragment) {
+}
