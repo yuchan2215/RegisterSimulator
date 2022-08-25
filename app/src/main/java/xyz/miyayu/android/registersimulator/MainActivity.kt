43efaaff4449256d622082b3cb3ba1c6756caec1
Miyayu <yuchan2215@gmail.com>
2022-08-25 18:36:55 +0900

    add: AppBarの表示を変更

--- app/src/main/java/xyz/miyayu/android/registersimulator/MainActivity.kt
+++ app/src/main/java/xyz/miyayu/android/registersimulator/MainActivity.kt
@@ -2,10 +2,29 @@ package xyz.miyayu.android.registersimulator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<MaterialToolbar>(R.id.tool_bar)
+            .setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
