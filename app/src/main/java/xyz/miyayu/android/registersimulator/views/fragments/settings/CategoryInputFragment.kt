f703aa1661618d38afbe5da2c7b7cdde9b32e717
Miyayu <yuchan2215@gmail.com>
2022-08-31 19:45:04 +0900

    add: リファクタリングをした

--- app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/settings/CategoryInputFragment.kt
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/settings/CategoryInputFragment.kt
@@ -15,7 +15,7 @@ import xyz.miyayu.android.registersimulator.viewmodel.CategoryInputViewModel
 
 class CategoryInputFragment : Fragment(R.layout.category_input_fragment) {
     private val navigationArgs: CategoryInputFragmentArgs by navArgs()
-    private val viewmodel: CategoryInputViewModel by viewModels {
+    private val viewModel: CategoryInputViewModel by viewModels {
         CategoryInputViewModel.Factory(navigationArgs)
     }
 
@@ -23,22 +23,45 @@ class CategoryInputFragment : Fragment(R.layout.category_input_fragment) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = CategoryInputFragmentBinding.bind(view)
+
+        /**
+         * カテゴリのタイトルが変わった時のリスナー
+         */
+        val titleChangedListener = object : SimpleTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+                viewModel.setCategoryName(s.toString())
+            }
+        }
+
+        /**
+         * ラジオボタンの値が変わった時のリスナー
+         */
+        val radioChangedListener =
+            RadioGroup.OnCheckedChangeListener { _, checkedId ->
+                val taxRateId = getTaxRateIdFromLayoutId(checkedId, binding)
+                viewModel.setCategoryTaxRate(taxRateId)
+            }
+
         //税率を読み込む
-        viewmodel.taxRates.observe(viewLifecycleOwner) {
+        viewModel.taxRates.observe(viewLifecycleOwner) {
             if (it == null) return@observe
 
+            //税率のテキストを表示する
             val bindTexts = listOf(binding.categoryTax1, binding.categoryTax2, binding.categoryTax3)
             bindTexts.forEachIndexed { index, button ->
                 val taxRate = it[index]
                 button.text = getString(R.string.category_tax_preview, taxRate.title, taxRate.rate)
             }
 
-            viewmodel.taxRates.removeObservers(viewLifecycleOwner)
+            //オブザーバーを削除する
+            viewModel.taxRates.removeObservers(viewLifecycleOwner)
+
+            //表示する
             binding.categoryTaxGroup.visibility = View.VISIBLE
         }
 
         //カテゴリを読み込む
-        viewmodel.category.observe(viewLifecycleOwner) {
+        viewModel.category.observe(viewLifecycleOwner) {
             if (it == null) return@observe
 
             // チェックした時のアニメーションを無効化する
@@ -47,37 +70,32 @@ class CategoryInputFragment : Fragment(R.layout.category_input_fragment) {
             }
             binding.categoryTaxGroup.setOnCheckedChangeListener(animationClearListener)
 
+            //初期値を入れる
             binding.categoryNameInput.setText(it.name)
-            binding.categoryTaxGroup.check(getTaxRateLayoutId(it.defaultTaxRateId))
+            binding.categoryTaxGroup.check(getTaxRateLayoutIdFromTaxRateId(it.defaultTaxRateId))
+
+            //アニメーションを再度有効にする
             binding.categoryNameLayout.isHintAnimationEnabled = true
 
-            viewmodel.category.removeObservers(viewLifecycleOwner)
-
-            val titleChangedListener = object : SimpleTextWatcher() {
-                override fun afterTextChanged(s: Editable?) {
-                    viewmodel.setCategoryName(s.toString())
-                }
-            }
-
-            val radioChangedListener =
-                RadioGroup.OnCheckedChangeListener { _, checkedId ->
-                    val taxRateId = getTaxRateId(checkedId, binding)
-                    viewmodel.setCategoryTaxRate(taxRateId)
-                }
-
+            //リスナーを設定する
             binding.categoryNameInput.addTextChangedListener(titleChangedListener)
             binding.categoryTaxGroup.setOnCheckedChangeListener(radioChangedListener)
+
+            //オブザーバーを削除する
+            viewModel.category.removeObservers(viewLifecycleOwner)
+
+            //表示する
             binding.categoryNameLayout.visibility = View.VISIBLE
         }
-        
-        //セーブできるか
-        viewmodel.canSave.observe(viewLifecycleOwner) {
+
+        //セーブできるかどうかを反映させる
+        viewModel.canSave.observe(viewLifecycleOwner) {
             binding.saveButton.isEnabled = it
         }
 
         //セーブボタンの処理
         binding.saveButton.setOnClickListener {
-            viewmodel.save()
+            viewModel.save()
             view.findNavController().popBackStack()
         }
         //キャンセルボタンの処理
@@ -86,7 +104,10 @@ class CategoryInputFragment : Fragment(R.layout.category_input_fragment) {
         }
     }
 
-    private fun getTaxRateId(taxRateLayoutId: Int?, binding: CategoryInputFragmentBinding): Int {
+    private fun getTaxRateIdFromLayoutId(
+        taxRateLayoutId: Int?,
+        binding: CategoryInputFragmentBinding
+    ): Int {
         return when (taxRateLayoutId) {
             binding.categoryTax1.id -> 1
             binding.categoryTax2.id -> 2
@@ -95,7 +116,7 @@ class CategoryInputFragment : Fragment(R.layout.category_input_fragment) {
         }
     }
 
-    private fun getTaxRateLayoutId(taxRateId: Int?): Int {
+    private fun getTaxRateLayoutIdFromTaxRateId(taxRateId: Int?): Int {
         return when (taxRateId) {
             1 -> R.id.category_tax_1
             2 -> R.id.category_tax_2
