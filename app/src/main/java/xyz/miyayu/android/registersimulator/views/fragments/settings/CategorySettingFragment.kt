7c2a7e3b004b3a22eec17a5b6e43ba2bb047c98d
Miyayu <yuchan2215@gmail.com>
2022-08-31 14:37:12 +0900

    add: カテゴリ設定画面のフラグメントを作成

---
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/settings/CategorySettingFragment.kt
@@ -0,0 +1,7 @@
+package xyz.miyayu.android.registersimulator.views.fragments.settings
+
+import androidx.fragment.app.Fragment
+import xyz.miyayu.android.registersimulator.R
+
+class CategorySettingFragment : Fragment(R.layout.category_setting_fragment) {
+}
