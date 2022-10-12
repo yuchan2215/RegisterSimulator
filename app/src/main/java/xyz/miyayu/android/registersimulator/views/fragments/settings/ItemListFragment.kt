6b9b809260be61b8ecebdd7043048796108e19fb
Miyayu <yuchan2215@gmail.com>
2022-10-12 15:56:56 +0900

    add: アイテム一覧画面の実装と推移を作成

---
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/settings/ItemListFragment.kt
@@ -0,0 +1,6 @@
+package xyz.miyayu.android.registersimulator.views.fragments.settings
+
+import androidx.fragment.app.Fragment
+import xyz.miyayu.android.registersimulator.R
+
+class ItemListFragment : Fragment(R.layout.item_list_fragment)
