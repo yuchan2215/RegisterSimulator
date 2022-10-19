afce883acec4d6ddbf349a9cafecc109e069a860
Miyayu <yuchan2215@gmail.com>
2022-10-19 10:56:12 +0900

    add: TaxRateをParelableにした

--- app/src/main/java/xyz/miyayu/android/registersimulator/model/entity/TaxRate.kt
+++ app/src/main/java/xyz/miyayu/android/registersimulator/model/entity/TaxRate.kt
@@ -1,19 +1,22 @@
 package xyz.miyayu.android.registersimulator.model.entity
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
 /**
  * 税金のレートを保持するためのクラス
  */
+@Parcelize
 @Entity(tableName = "taxes")
 data class TaxRate(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
     val title: String,
     val rate: String
-) {
+) : Parcelable {
     fun getBigDecimalRate(): BigDecimal {
         return BigDecimal(rate)
     }
