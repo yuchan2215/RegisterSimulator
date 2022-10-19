349e2acf5b21790e6af0455d9365b61392526f42
Miyayu <yuchan2215@gmail.com>
2022-10-19 13:22:54 +0900

    add: 商品一覧ページのバインディングを実装

--- app/src/main/java/xyz/miyayu/android/registersimulator/views/adapter/ItemListAdapter.kt
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/adapter/ItemListAdapter.kt
@@ -2,14 +2,19 @@ package xyz.miyayu.android.registersimulator.views.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import xyz.miyayu.android.registersimulator.R
 import xyz.miyayu.android.registersimulator.databinding.ItemSettingItemBinding
 import xyz.miyayu.android.registersimulator.model.entity.ProductItemDetail
+import xyz.miyayu.android.registersimulator.util.DecimalUtils.getSplitString
+import xyz.miyayu.android.registersimulator.util.ResourceService
 
-abstract class ItemListAdapter :
+abstract class ItemListAdapter(private val resourceService: ResourceService) :
     ListAdapter<ProductItemDetail, ItemListAdapter.ItemViewHolder>(DiffCallback) {
+
     abstract fun onItemClicked(item: ProductItemDetail)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         return ItemViewHolder(
@@ -31,6 +36,22 @@ abstract class ItemListAdapter :
         fun bind(item: ProductItemDetail) {
             binding.apply {
                 itemTitle.text = item.item.itemName
+
+                janCode.isVisible = item.item.janCode != null
+                janCode.text = item.item.janCode.toString()
+
+                categoryName.text = item.defaultCategoryDetail.category.name
+                price.text = resourceService.getResources().getString(
+                    R.string.without_tax_price,
+                    item.item.getBigDecimalPrice().getSplitString()
+                )
+                priceWithTax.text = resourceService.getResources().getString(
+                    R.string.price_preview,
+                    item.getTaxIncludedPrice().getSplitString()
+                )
+                errorText.isVisible = item.taxRate != null
+                errorText.text =
+                    resourceService.getResources().getString(R.string.category_override_message)
             }
         }
     }
--- app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/settings/ItemListFragment.kt
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/settings/ItemListFragment.kt
@@ -11,15 +11,20 @@ import dagger.hilt.android.AndroidEntryPoint
 import xyz.miyayu.android.registersimulator.R
 import xyz.miyayu.android.registersimulator.databinding.ItemListFragmentBinding
 import xyz.miyayu.android.registersimulator.model.entity.ProductItemDetail
+import xyz.miyayu.android.registersimulator.util.ResourceService
 import xyz.miyayu.android.registersimulator.viewmodel.ItemListViewModel
 import xyz.miyayu.android.registersimulator.views.adapter.ItemListAdapter
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ItemListFragment : Fragment(R.layout.item_list_fragment) {
+    @Inject
+    lateinit var resourceService: ResourceService
+
     private val viewModel: ItemListViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = object : ItemListAdapter() {
+        val adapter = object : ItemListAdapter(resourceService) {
             override fun onItemClicked(item: ProductItemDetail) {
                 // TODO
             }
