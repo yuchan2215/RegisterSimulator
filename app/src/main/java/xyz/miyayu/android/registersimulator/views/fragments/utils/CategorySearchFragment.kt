97c149d5039357f019637f26e3ff5a397d701314
Miyayu <yuchan2215@gmail.com>
2022-10-19 11:31:55 +0900

    add: カテゴリ選択画面を作成した

---
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/utils/CategorySearchFragment.kt
@@ -0,0 +1,6 @@
+package xyz.miyayu.android.registersimulator.views.fragments.utils
+
+import androidx.fragment.app.Fragment
+import xyz.miyayu.android.registersimulator.R
+
+class CategorySearchFragment : Fragment(R.layout.category_search_fragment)
