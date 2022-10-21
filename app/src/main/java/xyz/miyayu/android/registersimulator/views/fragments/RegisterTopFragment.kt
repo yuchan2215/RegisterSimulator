1d580c74959f6b58ffff729676bc76895d03d1cf
Miyayu <yuchan2215@gmail.com>
2022-10-21 11:13:24 +0900

    BarcodeTopFragmentを修正

--- app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/RegisterTopFragment.kt
+++ app/src/main/java/xyz/miyayu/android/registersimulator/views/fragments/RegisterTopFragment.kt
@@ -6,27 +6,27 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.journeyapps.barcodescanner.BarcodeCallback
 import com.journeyapps.barcodescanner.BarcodeResult
-import com.journeyapps.barcodescanner.CompoundBarcodeView
 import xyz.miyayu.android.registersimulator.R
 import xyz.miyayu.android.registersimulator.databinding.RegisterTopFragmentBinding
+import xyz.miyayu.android.registersimulator.views.view.ReaderView
 
 class RegisterTopFragment : Fragment(R.layout.register_top_fragment), BarcodeCallback {
-    private var _barcodeView: CompoundBarcodeView? = null
+    private var _barcodeView: ReaderView? = null
     private val barcodeView get() = _barcodeView!!
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val binding = RegisterTopFragmentBinding.bind(view)
-        _barcodeView = binding.barcodeView
-        barcodeView.barcodeView.decodeContinuous(this)
+        _barcodeView = binding.readerView
+        binding.readerView.setBarcodeCallback(this)
     }
 
     override fun onResume() {
         super.onResume()
-        barcodeView.resume()
+        barcodeView.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-        barcodeView.pause()
+        barcodeView.onPause()
     }
 
     override fun onDestroyView() {
