392711cc3d4650ef8197b565093f501695198fe3
Miyayu <yuchan2215@gmail.com>
2022-10-24 09:21:53 +0900

    remove: 利用していなかった関数を削除

--- app/src/main/java/xyz/miyayu/android/registersimulator/model/room/SettingRoomDatabase.kt
+++ app/src/main/java/xyz/miyayu/android/registersimulator/model/room/SettingRoomDatabase.kt
@@ -1,8 +1,6 @@
 package xyz.miyayu.android.registersimulator.model.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import xyz.miyayu.android.registersimulator.model.dao.CategoryDao
@@ -24,21 +22,4 @@ abstract class SettingRoomDatabase : RoomDatabase() {
     abstract fun taxRateDao(): TaxRateDao
     abstract fun categoryDao(): CategoryDao
     abstract fun productItem(): ProductItemDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: SettingRoomDatabase? = null
-        fun getDatabase(context: Context): SettingRoomDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    SettingRoomDatabase::class.java,
-                    "room_database"
-                ).fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
 }
